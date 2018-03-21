7188f50c7f1f1465565a3c5a6535b31efead7fdc
TimGallin <timgallin@outlook.com>
2018-03-21 16:53:08 +0800

    Add socket init.

---
+++ src/kangaro.h
@@ -0,0 +1,17 @@
+#ifndef KANGARO_MAIN_H
+#define KANGARO_MAIN_H
+
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <errno.h>
+#include <unistd.h>
+
+#define KANGARO_PORT "50203"
+#define KANGARO_BACKLOG 50
+
+
+
+
+#endif 
